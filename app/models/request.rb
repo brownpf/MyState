7feff16a5ced657d6d868ac1a39aa2647c9268a2
Alex Dollery <alexdollery@yahoo.co.uk>
2014-02-16 14:51:42 +0000

    prepped request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,17 @@
 class Request < ActiveRecord::Base
 
+  def reverse_geolocation(postcode)
+
+  end
+
+  def store_coords(x_coords, y_coords)
+
+  end
+
+  def store_status(status)
+
+  end
+
+
+
 end
