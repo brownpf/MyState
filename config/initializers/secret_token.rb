e67bb4c832e8474d7c7beffeec026e2936fbe539
Paul Brown <paul@brownpf.com>
2014-02-16 12:47:29 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StausMe::Application.config.secret_key_base = '09f04b72b58001fe8451f3a9baf84f281ca1f50a3c59e7b667aa4fe2e5a88f3e5b6bb14cfab823acc551abfccf0b87982b8cf2e825a5482c040112af7f5cb92a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StausMe::Application.config.session_store :cookie_store, key: '_staus_me_session'
