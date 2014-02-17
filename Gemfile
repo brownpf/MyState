9084b1025109c95eca3b8b154cf2281ae793ddf3
Paul Brown <paul@brownpf.com>
2014-02-17 10:36:30 +0000

    Working Twilio integration

--- Gemfile
+++ Gemfile
@@ -28,9 +28,6 @@ gem 'foundation-rails'
 
 gem 'sqlite3', group: :development
 
-
-gem 'pg', group: :production
-
 #Database Postgres
 #gem "pg", group: :production
 
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,24 +1,33 @@
 class Request < ActiveRecord::Base
+  # Capture the pattern
+  POSTCODE_REGEX = "^(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})$"
+
   require 'twilio-ruby'
 
+  # Instance method to capture the incoming SMS
   def store_sms (body, from)
-    if body.downcase != ''
+    # If the body isn't empty
+    if body.strip != ''
       message = ''
 
-      if body.length = 1
-        req = Request.find_by(telephone: from)
+      # Work out if it is a postcode
+      if body.length == 1
+        req = self.class.find_by(telephone: from)
 
         # Update that record
         store_status(body.to_i)
 
         #Set the message
         message = 'We have updated your status successfully'
-      elsif is_postcode(body)
+      elsif self.class.valid_postcode?(body)
+        # Store the postcode
+        self.postcode = body
+
         # Call the Geo-location
-        cord = Request.reverse_geolocation(body)
+        cord = self.reverse_geolocation(body)
 
         # Store the Geo
-        Request.store_coords(@cord[0],@cord[1])
+        self.store_coords(cord[:lat],cord[:lon])
 
         # Store the incomming number
         self.telephone = from
@@ -38,33 +47,41 @@ class Request < ActiveRecord::Base
     end
   end
 
-  def self.is_postcode(code)
-    regex = "^(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})$"
-    pattern = Regexp.new(regex).freeze
-    code =~ pattern
-  end
-
+  # Do a geo-location reverse look-up on PostCode
   def reverse_geolocation(postcode)
+    # API URL for google geo
     api_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{URI.encode(postcode)}&key=AIzaSyDFNZW_WcnVprGhOTWBDIPYsqCvLB9KHoE&sensor=false"
 
     # Grab the response
     res = HTTParty.get(api_url)
 
-    # Grab the lat and long
+    # Grab the lat and long from the JSON response
     lat = res["results"][0]["geometry"]["location"]["lat"]
     lon = res["results"][0]["geometry"]["location"]["lng"]
 
-    [lat,lon]
+    # Return the results to the calling method
+    {lat: lat,lon: lon}
   end
 
+  # Instance method which saves the lat and long co-ordinates
   def store_coords(x_coords, y_coords)
     self.latitude = x_coords
     self.longitude = y_coords
     self.save
   end
 
+  # Instance method which stores the status from an incomming request
   def store_status(status)
     self.status = status
     self.save
+  end
+
+  # Class method to validate the postcode passed in the body of the message
+  def self.valid_postcode?(code)
+    # Now load it for comparison
+    pattern = Regexp.new(POSTCODE_REGEX).freeze
+
+    # If its a match, then the postcode is valid, otherwise not
+    (code.upcase =~ pattern).nil? ? false:true
   end
 end
